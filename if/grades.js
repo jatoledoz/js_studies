4ce47224d8833fead38e53dc9b3a280811f2d8c2
Jorge <jorge@team4u.com.br>
2022-03-27 19:03:00 -0300

    Working with if/else

---
+++ if/grades.js
@@ -0,0 +1,8 @@
+var grade = prompt("Digite a nota do aluno:")
+var mean = 7
+
+if (grade >= mean) {
+  document.write("<hr/>Aluno está aprovado!")
+} else {
+  document.write("<hr/>Aluno está reprovado!")
+}
---
+++ if/if.js
@@ -0,0 +1,89 @@
+if (2 == 2) {
+  document.write("</br>if is true. 2 == 2")
+} else {
+  document.write("</br>if is false. else is true")
+}
+
+if (2 == 3) {
+  document.write("</br>if is true. 2 == 2")
+} else {
+  document.write("</br>if is false. else is true. 2 == 3")
+}
+
+if (2 == '2') {
+  document.write("</br>if is true. 2 == '2'")
+} else {
+  document.write("</br>if is false. else is true. 2 === '2'")
+}
+
+if (2 === 2) {
+  document.write("</br>if is true. 2 === 2")
+} else {
+  document.write("</br>if is false. else is true.")
+}
+
+if (2 === '2') {
+  document.write("</br>if is true. 2 === 2")
+} else {
+  document.write("</br>if is false. else is true. 2 === '2'")
+}
+
+if (2 != '2') {
+  document.write("</br>if is true. 2 == '2'")
+} else {
+  document.write("</br>if is false. else is true. 2 != '2'")
+}
+
+if (2 !== 3) {
+  document.write("</br>if is true. 2 !== 3")
+} else {
+  document.write("</br>if is false. else is true. ")
+}
+
+if (2 < 3) {
+  document.write("</br>if is true. 2 < 3")
+} else {
+  document.write("</br>if is false. else is true.")
+}
+
+if (2 < 1) {
+  document.write("</br>if is true.")
+} else {
+  document.write("</br>if is false. else is true. 2 < 1")
+}
+
+if (2 > 1) {
+  document.write("</br>if is true. 2 > 1")
+} else {
+  document.write("</br>if is false. else is true.")
+}
+
+if (2 > 2) {
+  document.write("</br>if is true.")
+} else {
+  document.write("</br>if is false. else is true. 2 > 2")
+}
+
+if (2 <= 2) {
+  document.write("</br>if is true. 2 <= 2")
+} else {
+  document.write("</br>if is false. else is true.")
+}
+
+if (2 <= 1) {
+  document.write("</br>if is true.")
+} else {
+  document.write("</br>if is false. else is true. 2 <= 1")
+}
+
+if (2 >= 2) {
+  document.write("</br>if is true. 2 >= 2")
+} else {
+  document.write("</br>if is false. else is true.")
+}
+
+if (2 >= 3) {
+  document.write("</br>if is true.")
+} else {
+  document.write("</br>if is false. else is true. 2 >= 3")
+}
