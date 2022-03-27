1fab328eb5ce124dbf638def52556ea67c8ac272
Jorge <jorge@team4u.com.br>
2022-03-27 18:25:20 -0300

    About variables and how to use them

---
+++ Variables/variables.js
@@ -0,0 +1,29 @@
+//string
+var text = "JavaScript studies"
+
+//number
+var intNumber = 55 //int
+var floatNumber = 10.7 //float
+
+//boolean
+var bool = true
+
+document.write(text + '</br>')
+document.write(intNumber + '</br>')
+document.write(floatNumber + '</br>')
+document.write(bool + '</br>')
+
+//Inputing values on variable through prompt
+var username = prompt('Digite o seu nome:')
+var age = prompt('Digite a sua idade:')
+document.write('<h1>Nome: ' + username + '.</br> Idade: ' + age + ' anos.</h1>')
+
+//null
+  var varNull = null //Intentionally defining as empty
+
+//undefined
+  var varUndefined //Value was not defined
+
+//Changing values on variables
+  username = "Alterando o nome do usuário"
+  document.write('<h1>' + username + '</h1>')
